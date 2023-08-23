970dedb3dee9437965d634fcdb597080f2e37488
AngieVal122 <142047070+AngieVal122@users.noreply.github.com>
2023-08-22 19:27:36 -0500

    Update LETRA.java

--- src/LETRA.java
+++ src/LETRA.java
@@ -1,27 +1,23 @@
 import java.util.Scanner;
 
-public class LETRA {
+public class NUMEROLETRA {
+    public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-          public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-
-            System.out.print("Ingrese una letra o un numero en mayuscula ");
-            char numeroLetra = sc.next().charAt(0);
-
-
-            if (Character.isLetter(numeroLetra)) {
-                if (Character.isUpperCase(numeroLetra)) {
+            System.out.print("Ingrese un carácter: ");
+            char letraNumero = scanner.next().charAt(0);
+            int acsii = (int)letraNumero;
+            if (Character.isLetter(letraNumero)) {
+                if (letraNumero>=65 && letraNumero<=90) {
                     System.out.println("El carácter es una letra mayúscula.");
                 } else {
                     System.out.println("El carácter es una letra minúscula.");
                 }
-            } else if (Character.isDigit(numeroLetra)) {
+            } else if (letraNumero>=48 && letraNumero<=57) {
                 System.out.println("El carácter es un número.");
             } else {
                 System.out.println("El carácter no es una letra ni un número.");
             }
 
-
         }
-    }
-
+}
